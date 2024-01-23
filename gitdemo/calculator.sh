8a0a5faf7db37aa30b91a697fdd852cd0446862a
SumeetK42 <ubuntu@ip-172-31-10-70.ap-south-1.compute.internal>
2024-01-23 09:43:17 +0000

    build cal and gitdemo

---
+++ gitdemo/calculator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##############################
+## Author : Sumeet ####
+## Date : 23 Jan 2023 ##
+###################################
+
+
+## addition function
+A=4
+SUM=$A+3
+echo "Sum is " 
+echo $SUM
+
